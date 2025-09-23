e9d7940c81a0068a6f089709c819e5a6fda75e3e
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-09-23 12:23:15 +0800

    build: Build amd64 and arm64 multi-platform Docker image
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- publish-docker.sh
+++ publish-docker.sh
@@ -10,16 +10,9 @@ if [[ $VERSION = *"SNAPSHOT"* ]]; then
   echo "Do not publish a snapshot version of dependency-check"
   exit 1
 fi
-docker inspect --type=image owasp/dependency-check:$VERSION  > /dev/null 2>&1
-if [[ "$?" -ne 0 ]] ; then
-  echo "docker image owasp/dependency-check:$VERSION does not exist - run build_docker.sh first"
-  exit 1
-fi
-docker inspect --type=image owasp/dependency-check:latest  > /dev/null 2>&1
-if [[ "$?" -ne 0 ]] ; then
-  echo "docker image owasp/dependency-check:latest does not exist - run build_docker.sh first"
-  exit 1
-fi
 
-docker push owasp/dependency-check:$VERSION 
-docker push owasp/dependency-check:latest
+# Build args should match ./build-docker.sh so the builder cache is re-used
+docker buildx build --push --platform linux/amd64,linux/arm64 . \
+  --build-arg VERSION=$VERSION \
+  --tag owasp/dependency-check:$VERSION \
+  --tag owasp/dependency-check:latest
